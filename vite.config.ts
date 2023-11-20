e9b942c56dbc32e525d446c6e3a45cffa971674c
Florian Imdahl <git@ffflorian.de>
2023-11-20 21:51:50 +0100

    fix: Production base

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import eslint from 'vite-plugin-eslint';
 
-export default defineConfig(({}) => {
+export default defineConfig(({mode}) => {
   return {
+    base: mode === 'production' ? '/sauerteig' : '',
     plugins: [
       react(),
       {
